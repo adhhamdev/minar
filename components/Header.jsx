d597ea1e0fd4fc331aee8ff883b9f3ba22ed1fb2
Adhham Safwan <msmadhhamg@gmail.com>
2024-09-20 02:55:20 +0530

    Refactor manifest.json and Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ isSettingsOpen, setIsSettingsOpen }) {
   return (
     <div className='flex items-center justify-between mb-6'>
       <h1 className='flex items-center text-2xl font-bold text-emerald-600'>
-        <MoonStar className='w-4 h-4 mr-2' />
+        <MoonStar className='w-5 h-5 mr-2' />
         Minar
       </h1>
       <Button
